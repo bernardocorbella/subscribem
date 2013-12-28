f341ba9e2d8e17b001389f37b811bacff801dded
Bernardo <bernardo@insignia4u.com>
2013-12-28 19:54:16 -0300

    Finishes section 2.5: Adding subdomain support

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,6 +1,18 @@
 module Subscribem
   class Account < ActiveRecord::Base
+    EXCLUDED_SUBDOMAINS = %w(admin)
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
+
     belongs_to :owner, class_name: "Subscribem::User"
+
     accepts_nested_attributes_for :owner
+
+    validates :subdomain, presence: true, uniqueness: true
+    validates_format_of :subdomain, with: /\A[\w\-]+\Z/i, #letters, numbers, underscore and dashes.
+      message: "is not allowed. Please choose another subdomain."
+    validates_exclusion_of :subdomain, in: EXCLUDED_SUBDOMAINS,
+      message: "is not allowed. Please choose another subdomain."
   end
 end
